4c055757f341bab2b1cf835e0ec802c08f27b8f8
monachhabra <monachhabra5@gmail.com>
2021-10-10 21:12:39 +0530

    Added src folder

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//import './App.css';
+import Forms from "./forms";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+       {/* <p>
+          Learn React
+        </p> */}
+        <Forms/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
